e3b7dba686ee4770fb2411c87669e8a7a04bd23c
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-03-04 13:56:24 +0530

    initialize React + TypeScript + Vite project with essential configurations and files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-blue-200">
+      <h1 className="text-4xl font-bold text-white">
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
