e759eec0b0d9cb11864fea30eb798215d34301ef
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-03-06 20:19:56 +0530

    add: implement trigger to update 'updated_at' column on notes and create NoteCard component

--- supabase/migrations/20250303161312_fading_surf.sql
+++ supabase/migrations/20250303161312_fading_surf.sql
@@ -63,3 +63,17 @@ CREATE POLICY "Users can view their own profile"
   TO authenticated
   USING (auth.uid() = id)
   WITH CHECK (auth.uid() = id);
+
+  -- Create trigger to update the updated_at column
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = now();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_notes_updated_at
+BEFORE UPDATE ON notes
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
