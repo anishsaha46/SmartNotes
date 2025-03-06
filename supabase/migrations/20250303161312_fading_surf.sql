1a2f4ffb645bdb53a61fd1e1ba0ba72658e364e7
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-03-06 20:18:24 +0530

    add: create NoteCard component for displaying notes

--- supabase/migrations/20250303161312_fading_surf.sql
+++ supabase/migrations/20250303161312_fading_surf.sql
@@ -56,3 +56,10 @@ CREATE POLICY "Users can view their own profile"
   FOR SELECT
   TO authenticated
   USING (auth.uid() = id);
+
+  CREATE POLICY "Users can update their own profile"
+  ON profiles
+  FOR UPDATE
+  TO authenticated
+  USING (auth.uid() = id)
+  WITH CHECK (auth.uid() = id);
