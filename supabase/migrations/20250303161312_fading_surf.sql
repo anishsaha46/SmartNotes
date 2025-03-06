86a56ca3619a7ec37d5fe40329ab7ab7d970fc4a
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-03-06 20:16:52 +0530

    add: create policy for users to view their own notes in Row Level Security

--- supabase/migrations/20250303161312_fading_surf.sql
+++ supabase/migrations/20250303161312_fading_surf.sql
@@ -36,3 +36,9 @@ CREATE POLICY "Users can create their own notes"
   FOR INSERT
   TO authenticated
   WITH CHECK (auth.uid() = user_id);
+
+  CREATE POLICY "Users can view their own notes"
+  ON notes
+  FOR SELECT
+  TO authenticated
+  USING (auth.uid() = user_id);
