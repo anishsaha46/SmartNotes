bb5d01b053bc03b88c10346d37570939ae47bdf9
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-03-09 10:20:55 +0530

    fetch notes on component mount in NoteList

--- src/components/notes/NoteList.tsx
+++ src/components/notes/NoteList.tsx
@@ -1,12 +1,18 @@
+import React,{useState,useEffect} from 'react';
 import { Link } from "react-router-dom";
 import { useNoteStore } from "../../store/noteStore";
 import NoteCard from "./NoteCard";
 import { Plus,Search,Star } from "lucide-react";
 import Button from "../ui/Button";
+import { useEffect } from "react";
 
 
 const NoteList: React.FC=()=>{
     const {notes,fetchNotes,loading}=useNoteStore();
     const [searchTerm,setSearchTerm]=useState('');
     const [showFavorites,setShowFavorites]=useState(false);
+
+    useEffect(()=>{
+        fetchNotes();
+    },[fetchNotes]);
 }
