802441cc0f2ef0a446a6b021b6466cd04aa2ade7
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-03-10 14:22:26 +0530

    add NewNotePage component to render NoteForm for creating new notes

---
+++ src/pages/notes/NewNotePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import NoteForm from '../../components/notes/NoteForm';
+
+const NewNotePage: React.FC = () => {
+  return <NoteForm />;
+};
+
+export default NewNotePage;
