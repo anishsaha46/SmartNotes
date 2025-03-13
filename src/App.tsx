87dc2ba2f34cf07f9e7726ee4160a5f633572ab3
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-03-13 15:03:48 +0530

    refactor App component for improved code organization and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import './index.css'
-import React,{Children, useEffect} from 'react'
-import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom'
-import { useAuthStore } from './store/authStore'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthStore } from './store/authStore';
 
 // Layout
-import Layout from './components/layout/Layout'
-
+import Layout from './components/layout/Layout';
 
 // Pages
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import NotesPage from './pages/notes/NotesPage'
-import NoteDetailPage from './pages/notes/NoteDetailPage'
-import NewNotePage from './pages/notes/NewNotePage'
-import EditNotePage from './pages/notes/EditNotePage'
-import NotFoundPage from './pages/NoteFoundPage'
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import NotesPage from './pages/notes/NotesPage';
+import NoteDetailPage from './pages/notes/NoteDetailPage';
+import NewNotePage from './pages/notes/NewNotePage';
+import EditNotePage from './pages/notes/EditNotePage';
+import NotFoundPage from './pages/NoteFoundPage';
 
 // Protected Route Component
-
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { user, loading } = useAuthStore();
   
@@ -38,12 +35,12 @@ const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) =
 };
 
 function App() {
-
-  const {getUser}=useAuthStore();
+  const { getUser } = useAuthStore();
+  
   useEffect(() => {
     getUser();
-  }, [getUser])
-
+  }, [getUser]);
+  
   return (
     <BrowserRouter>
       <Routes>
@@ -51,33 +48,35 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
-
-
+          
           {/* Protected Routes */}
           <Route path="notes" element={
             <ProtectedRoute>
               <NotesPage />
             </ProtectedRoute>
           } />
-
           <Route path="notes/:id" element={
             <ProtectedRoute>
               <NoteDetailPage />
             </ProtectedRoute>
           } />
-
-<Route path="notes/new" element={
+          <Route path="notes/new" element={
             <ProtectedRoute>
               <NewNotePage />
             </ProtectedRoute>
           } />
-
-<Route path="notes/edit/:id" element={
+          <Route path="notes/edit/:id" element={
             <ProtectedRoute>
               <EditNotePage />
             </ProtectedRoute>
           } />
-  )
+          
+          {/* 404 Route */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
