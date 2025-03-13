c950edd79785289f3ec0996835f929c34c401b4c
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-03-13 14:53:20 +0530

    add Layout component import in App for improved structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import React,{useEffect} from 'react'
 import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom'
 import { useAuthStore } from './store/authStore'
 
+// Layout
+import Layout from './components/layout/Layout'
+
 
 function App() {
   return (
